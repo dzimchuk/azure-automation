a008b15976bf3d959561adb31f0cae450dbe29c0
Andrei Dzimchuk <andrei@dzimchuk.net>
2017-08-15 09:18:24 +0300

    added SQL database export and import scripts

---
+++ SQLDatabase/Export-SQLDatabase.ps1
@@ -0,0 +1,41 @@
+﻿param
+(
+  [Parameter(Mandatory = $true)]  
+  [string]$resourceGroupName,
+  [Parameter(Mandatory = $true)]  
+  [string]$serverName,
+  [Parameter(Mandatory = $true)]  
+  [string]$databaseName,
+  [Parameter(Mandatory = $true)]  
+  [string]$storageKeytype,
+  [Parameter(Mandatory = $true)]  
+  [string]$storageKey,
+  [Parameter(Mandatory = $true)]  
+  [string]$bacpacUri,
+  [Parameter(Mandatory = $true)]  
+  [string]$administratorLogin,
+  [Parameter(Mandatory = $true)]  
+  [string]$administratorLoginPassword
+)
+
+# Set the output level to verbose and make the script stop on error
+$VerbosePreference = "Continue"
+$ErrorActionPreference = "Stop"
+
+
+$exportRequest = New-AzureRmSqlDatabaseExport -ResourceGroupName $resourceGroupName -ServerName $serverName `
+  -DatabaseName $databaseName -StorageKeytype $storageKeytype -StorageKey $storageKey -StorageUri $bacpacUri `
+  -AdministratorLogin $administratorLogin -AdministratorLoginPassword $(ConvertTo-SecureString -String $administratorLoginPassword -AsPlainText -Force)
+
+$exportStatus = Get-AzureRmSqlDatabaseImportExportStatus -OperationStatusLink $exportRequest.OperationStatusLink
+[Console]::Write("Exporting")
+
+while ($exportStatus.Status -eq "InProgress")
+{
+    $exportStatus = Get-AzureRmSqlDatabaseImportExportStatus -OperationStatusLink $exportRequest.OperationStatusLink
+    [Console]::Write(".")
+    Start-Sleep -s 10
+}
+
+[Console]::WriteLine("")
+$exportStatus
---
+++ SQLDatabase/Import-SQLDatabase.ps1
@@ -0,0 +1,49 @@
+﻿param
+(
+  [Parameter(Mandatory = $true)]  
+  [string]$resourceGroupName,
+  [Parameter(Mandatory = $true)]  
+  [string]$serverName,
+  [Parameter(Mandatory = $true)]  
+  [string]$databaseName,
+  [Parameter(Mandatory = $true)]  
+  [string]$storageKeytype,
+  [Parameter(Mandatory = $true)]  
+  [string]$storageKey,
+  [Parameter(Mandatory = $true)]  
+  [string]$bacpacUri,
+  [Parameter(Mandatory = $true)]  
+  [string]$administratorLogin,
+  [Parameter(Mandatory = $true)]  
+  [string]$administratorLoginPassword
+)
+
+# Set the output level to verbose and make the script stop on error
+$VerbosePreference = "Continue"
+$ErrorActionPreference = "Stop"
+
+
+$importRequest = New-AzureRmSqlDatabaseImport -ResourceGroupName $resourceGroupName `
+   -ServerName $serverName `
+   -DatabaseName $databaseName `
+   -DatabaseMaxSizeBytes "262144000" `
+   -StorageKeyType $storageKeytype `
+   -StorageKey $storageKey `
+   -StorageUri $bacpacUri `
+   -Edition "Standard" `
+   -ServiceObjectiveName "P6" `
+   -AdministratorLogin $administratorLogin `
+   -AdministratorLoginPassword $(ConvertTo-SecureString -String $administratorLoginPassword -AsPlainText -Force)
+
+$importStatus = Get-AzureRmSqlDatabaseImportExportStatus -OperationStatusLink $importRequest.OperationStatusLink
+[Console]::Write("Importing")
+
+while ($importStatus.Status -eq "InProgress")
+{
+    $importStatus = Get-AzureRmSqlDatabaseImportExportStatus -OperationStatusLink $importRequest.OperationStatusLink
+    [Console]::Write(".")
+    Start-Sleep -s 10
+}
+
+[Console]::WriteLine("")
+$importStatus
